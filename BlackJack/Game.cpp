253ce6fc1cadbfb218065c6991f2ce11412853f7
mrjoeyrules <mrjoeyrules@gmail.com>
2022-03-22 12:55:37 +0000

    Game card dealer updated
    card dealer improved

--- BlackJack/Game.cpp
+++ BlackJack/Game.cpp
@@ -7,31 +7,58 @@
 #include "MainMenu.h"
 using namespace std;
 
+int playerCurrentCount = 0;
+int dealerCurrentCount = 0;
 int const MaxCardsPerDeck = 13;
 int const MaxCardsPerDeckAce = 14;
 int const MaxDecks = 4;
 string playerUserName;
 int playerChipBal;
+int cardValue[MaxCardsPerDeckAce] = { 1,2,3,4,5,6,7,8,9,10,10,10,11 };
 struct Decks
 {
 	std::vector<string> decks;
 };
 
+Decks deckMaker[MaxDecks];
+
 void Game::defineDeck()
 {
 	
-	int cardValue[MaxCardsPerDeckAce] = { 1,2,3,4,5,6,7,8,9,10,10,10,11 };
-	Decks deckMaker[MaxDecks];
 	for (size_t i = 0; i < MaxDecks; i++)
 	{
 		 deckMaker[i].decks = { "A","2","3","4","5","6","7","8","9","J","Q","K" };
-	}
-	
+	}	
 }
 
 void Game::dealCards()
 {
+	bool loopControl = true;
+	int randomDeck = 0;
+	int cardValueInner;
+	int randomCard = 0;
+	while (loopControl)
+	{
+		randomDeck = rand() % 4;
+		randomCard = rand() % 12;
+		if (randomCard == 0)
+		{
+			if (playerCurrentCount < 11)
+			{
+				cardValueInner = cardValue[12];
+			}
+			else
+			{
+				cardValueInner = cardValue[randomCard];
+			}
+		}
+		else
+		{
+			cardValueInner = cardValue[randomCard];
+		}
 
+	}
+	
 }
 
 void Game::playRound()
